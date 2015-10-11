3e7c633ecf885eab298284e0744b1437bb729843
spmuthu21 <muthu@globalensolutions.com>
2015-10-11 20:21:03 +0530

    deleted unnecessary files

--- events/events/models.py
+++ events/events/models.py
@@ -8,28 +8,15 @@ from college_event.models import *
 class Userprofile(models.Model):
     # This line is required. Links UserProfile to a User model instance.
     user = models.OneToOneField(User)
-    # The additional attributes we wish to include.
-    # website = models.URLField(blank=True)
-    # picture = models.ImageField(upload_to='static/img/', blank=True)
-
-    # city=models.CharField(max_length=50, null=True, blank=True)
     city=models.ForeignKey('college_event.City', null=True, blank=True)
-
     mobile=models.CharField(max_length=50)
     confirmation_code=models.CharField(max_length=500)
     is_subscribed=models.BooleanField(default=False)
-    # country=models.ForeignKey('college_event.Country',null=True, blank=True)
-    # language=models.CharField(max_length=5)
-    # address=models.OneToOneField(Address)
-    # age_status=models.BooleanField(default=False)
     is_emailverified=models.BooleanField(default=False)
-    # ad_count=models.IntegerField(max_length=5, null=True, blank=True)
-   
     lastname=models.CharField(max_length=50, null=True, blank=True)
     city=models.ForeignKey(City,null=True, blank=True)
     college=models.ForeignKey(College, null=True, blank=True)
     department=models.ForeignKey(Department, null=True, blank=True)
-
     # Override the __unicode__() method to return out something meaningful!
     def __unicode__(self):
         return self.user.username      
