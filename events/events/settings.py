f40bea769ef5863cfa53ddecbfc1b21bb1c4bd0c
root <root@s192-169-200-166.secureserver.net>
2015-10-13 07:35:01 -0700

    change in email password and image path

--- events/events/settings.py
+++ events/events/settings.py
@@ -167,7 +167,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'eventswat@gmail.com'
-EMAIL_HOST_PASSWORD = 'Street11'
+EMAIL_HOST_PASSWORD = 'street11'
 EMAIL_PORT = 587
 
 
